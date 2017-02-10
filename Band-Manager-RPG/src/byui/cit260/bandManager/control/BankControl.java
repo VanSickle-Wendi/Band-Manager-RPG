16a419838a88a5c2e99d3bf3fa7cda59cc6989aa
Polfer-Shannon <shannon.polfer@gmail.com>
2017-02-09 23:06:18 -0700

    02/09/17 Created 5 control classes, BankControlTest.java.

---
+++ Band-Manager-RPG/src/byui/cit260/bandManager/control/BankControl.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.bandManager.control;
+
+/**
+ *
+ * @author shaza
+ */
+public class BankControl {
+  public double calcLoanPayment(double loanBalance, double rate, double extraPrincipal) {
+          
+    if (loanBalance <= 0 || loanBalance > 25000) {
+        return -999;
+  }
+    if (rate != .025) {
+        return -999;
+  }
+ 
+    if (extraPrincipal < 0 || extraPrincipal > 25000){
+        return -999;
+  }
+ 
+    double loanPayment = (loanBalance * rate) + extraPrincipal; 
+        return loanPayment;
+           
+          
+}
+  
+}
+
+    
---
+++ Band-Manager-RPG/test/byui/cit260/bandManager/control/BankControlTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.bandManager.control;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author shaza
+ */
+public class BankControlTest {
+    
+    public BankControlTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcLoanPayment method, of class BankControl.
+     */
+    @Test
+    public void testCalcLoanPayment() {
+        System.out.println("calcLoanPayment");
+        double loanBalance = 1500;
+        double rate = .025;
+        double extraPrincipal = 500;
+        // Create instance of BankControl class
+        BankControl instance = new BankControl();
+        double expResult = 537.5;        
+        double result = instance.calcLoanPayment(loanBalance, rate, extraPrincipal);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
